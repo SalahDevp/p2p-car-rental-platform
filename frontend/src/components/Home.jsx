9817485dad8ade8d482369c4daba696eec598ae0
Salah Eddine Oukil <slhokli2002@gmail.com>
2024-05-26 21:12:44 +0100

    Update home page

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -5,8 +5,10 @@ export default function Home() {
   return (
     <div className="container home-page">
       <div className="home-title-box">
-        <h1 className="title">Rent your car</h1>
-        <h1 className="title-prefix">with Crypto!</h1>
+        <h1 className="title">
+          Rent the Perfect Car, Directly From Local Owners
+        </h1>
+        <h1 className="title-prefix">with no middlemen!</h1>
       </div>
       <div className="home-description-box">
         <p className="title-description">
@@ -15,7 +17,7 @@ export default function Home() {
         </p>
       </div>
       <Link to="/dashboard" className="button-class choose-car">
-        Our fleet
+        Get Started
       </Link>
     </div>
   );
